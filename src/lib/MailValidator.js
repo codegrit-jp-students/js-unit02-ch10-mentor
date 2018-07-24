f36a2329e4b6bf9583e9087ccab09e2639cf1f38
Atsuhiro Teshima <hiro@lingualbox.com>
2018-07-24 14:44:58 +0800

    initial commit

---
+++ src/lib/MailValidator.js
@@ -0,0 +1,27 @@
+import BaseValidator from './BaseValidator';
+
+export default class extends BaseValidator {
+  constructor(val) {
+    super(val, 'メールアドレス');
+    this.checkFormat = this.checkFormat.bind(this);
+  }
+
+  validate() {
+    return super.cannotEmpty()
+      .then(this.checkFormat)
+      .then(() => ({ success: true }))
+      .catch(err => err);
+  }
+
+  checkFormat() {
+    const re = /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i;
+    const match = re.test(this.val); // マッチするならtrue、しないならfalseを返す。
+    if (match) {
+      return Promise.resolve();
+    }
+    return Promise.reject({
+      success: false,
+      message: `${this.type}のフォーマットが異なります。`,
+    });
+  }
+}
---
+++ src/lib/PasswordValidator.js
@@ -0,0 +1,25 @@
+import BaseValidator from './BaseValidator';
+
+export default class extends BaseValidator {
+  constructor(val) {
+    super(val, 'パスワード');
+    this.checkLength = this.checkLength.bind(this);
+  }
+
+  validate() {
+    return super.cannotEmpty()
+      .then(this.checkLength)
+      .then(() => ({ success: true })) // Promise.resolve({ success: true })と同一
+      .catch(err => err); // Promise.resolve(err)と同一
+  }
+
+  checkLength() {
+    if (this.val.length >= 8) {
+      return Promise.resolve();
+    }
+    return Promise.reject({
+      success: false,
+      message: 'パスワードが短すぎます。',
+    });
+  }
+}
